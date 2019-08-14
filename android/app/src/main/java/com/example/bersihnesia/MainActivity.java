3306787d0cdcad00af285ec07ea58510d75a9cbc
Mohammad Ainun Najib <ainun.najib2403@gmail.com>
2019-08-14 12:59:56 +0700

    added

--- android/app/src/main/java/com/example/bersihnesia/MainActivity.java
+++ android/app/src/main/java/com/example/bersihnesia/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.bersihnesia;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.bersihnesia.activity.LoginActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void login(View view) {
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(intent);
     }
 }
