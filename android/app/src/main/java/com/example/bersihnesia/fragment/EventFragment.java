6fb097eafc7902dae0a2df95a0855a24adeac0e7
Mohammad Ainun Najib <ainun.najib2403@gmail.com>
2019-08-16 14:13:44 +0700

    added

--- android/app/src/main/java/com/example/bersihnesia/fragment/EventFragment.java
+++ android/app/src/main/java/com/example/bersihnesia/fragment/EventFragment.java
@@ -1,6 +1,7 @@
 package com.example.bersihnesia.fragment;
 
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
