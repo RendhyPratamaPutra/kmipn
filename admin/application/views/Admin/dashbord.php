26b64a7e4c1f8ced0943a2c75e02df1c759f9f53
Rendhy Pratama Putra <rendhypratama880@gmail.com>
2019-08-19 13:10:47 +0700

    personal2

--- admin/application/views/Admin/dashbord.php
+++ admin/application/views/Admin/dashbord.php
@@ -73,7 +73,7 @@
                                     </a>
                                 </li>
                                 <li>
-                                    <a href="bs-basic-table.html">
+                                    <a href="<?= base_url('personal/personal_form'); ?>">
                                         <span class="pcoded-micon"><i class="ti-layers"></i><b>FC</b></span>
                                         <span class="pcoded-mtext" data-i18n="nav.form-components.main">Komunitas</span>
                                         <span class="pcoded-mcaret"></span>
