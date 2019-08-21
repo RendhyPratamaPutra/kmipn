25faf3eb881fd230e019342cdcf6be71338a0c6c
ajipratama11 <47959982+ajipratama11@users.noreply.github.com>
2019-08-22 00:04:17 +0700

    v

--- admin/application/views/Admin/dashbord.php
+++ admin/application/views/Admin/dashbord.php
@@ -178,35 +178,40 @@
                             <!-- Data Event -->
                             <div class="table-responsive">
                                 <table class="table table-hover">
-                                  <thead>
-                                    <tr>
-                                      <th>#</th>
-                                      <th>First Name</th>
-                                      <th>Last Name</th>
-                                      <th>Username</th>
-                                    </tr>
-                                  </thead>
-                                  <tbody>
-                                      <tr>
-                                        <th scope="row">1</th>
-                                        <td>Mark</td>
-                                        <td>Otto</td>
-                                        <td>@mdo</td>
-                                      </tr>
-                                      <tr>
-                                        <th scope="row">2</th>
-                                        <td>Jacob</td>
-                                        <td>Thornton</td>
-                                        <td>@fat</td>
-                                      </tr>
-                                      <tr>
-                                        <th scope="row">3</th>
-                                        <td>Larry</td>
-                                        <td>the Bird</td>
-                                        <td>@twitter</td>
-                                      </tr>
-                                  </tbody>
-                              </table>
+                  
+													<thead>
+														<tr>
+															<th>No</th>
+															<th>Name Event</th>
+															<th>Address</th>
+															<th>Description</th>
+															<th>Time&Date</th>
+															<th>Photo</th>
+															<th>Status Event</th>
+				
+														</tr>
+													</thead>
+													<tbody>
+														<?php
+														$no=1;
+														foreach($dataevent as $e)
+														{
+														?>
+														<tr>
+															<th><?= $no++?></th>
+															<td><?= $e->name_event?></td>
+															<td><?= $e->address?></td>
+															<td><?= $e->description?></td>
+															<td><?= $e->time_date?></td>
+															<td><img src="<?php echo base_url('./upload/'.$e->photo) ?>"  width="100" /></td>
+															<td><?= $e->status_event?></td>
+								
+														</tr>
+														<?php
+														
+													}?>
+													</tbody>
+												</table>
                             </div>
                             <!-- end Data Event -->
 
